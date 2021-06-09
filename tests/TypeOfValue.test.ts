4f6ad8289a494834a4a2fe65b077f392799e9af0
Lou Cyx <git@lou.cx>
2021-06-08 19:26:20 -0700

    :white_check_mark: add tests for `TypeOfValue`.

---
+++ tests/TypeOfValue.test.ts
@@ -0,0 +1,19 @@
+import type { TypeOfValue } from "../src";
+
+export const bigintTypeOf: TypeOfValue = "bigint";
+export const booleanTypeOf: TypeOfValue = "boolean";
+export const functionTypeOf: TypeOfValue = "function";
+export const numberTypeOf: TypeOfValue = "number";
+export const objectTypeOf: TypeOfValue = "object";
+export const stringTypeOf: TypeOfValue = "string";
+export const symbolTypeOf: TypeOfValue = "symbol";
+export const undefinedTypeOf: TypeOfValue = "undefined";
+
+// @ts-expect-error TypeOfValue can't be empty.
+export const emptyTypeOf: TypeOfValue = "";
+
+// @ts-expect-error TypeOfValue should be one of the above.
+export const invalidTypeOf: TypeOfValue = "null";
+
+// @ts-expect-error TypeOfValue must be an string.
+export const nonStringTypeOf: TypeOfValue = undefined;
