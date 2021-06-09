3c941fea7988a437bfd5730a54a16f9f0d5d5256
Lou Cyx <git@lou.cx>
2021-06-08 18:25:21 -0700

    :white_check_mark: ReadOnlyRecord tests.

---
+++ tests/ReadOnlyRecord.test.ts
@@ -0,0 +1,15 @@
+import type { ReadOnlyRecord } from "../src";
+
+const record: ReadOnlyRecord<"foo", "foo"> = { foo: "foo" };
+
+// @ts-expect-error Mutations not allowed
+// eslint-disable-next-line functional/immutable-data, functional/no-expression-statement
+record.foo = "bar";
+
+// @ts-expect-error Mutations not allowed
+// eslint-disable-next-line functional/immutable-data, functional/no-expression-statement
+record.bar = "bar";
+
+// @ts-expect-error Mutations not allowed
+// eslint-disable-next-line functional/immutable-data, functional/no-expression-statement
+delete record.foo;
