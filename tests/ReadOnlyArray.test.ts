b12135d7632dccb945b784e483b8599d0e142d3b
Luke Shiru <hi@luke.sh>
2022-08-02 22:32:29 -0700

    :bookmark: version bump to v3.0.2.

--- tests/ReadOnlyArray.test.ts
+++ tests/ReadOnlyArray.test.ts
@@ -7,37 +7,37 @@ const array: ReadOnlyArray<string> = [];
 array[0] = "vangware";
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, functional/no-expression-statement
 array.copyWithin(0, 1);
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, functional/no-expression-statement
 array.fill("vangware");
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, functional/no-expression-statement
 array.pop();
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, functional/no-expression-statement
 array.push("vangware");
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, functional/no-expression-statement
 array.reverse();
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, functional/no-expression-statement
 array.shift();
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/require-array-sort-compare, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/require-array-sort-compare, functional/no-expression-statement
 array.sort();
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/require-array-sort-compare, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/require-array-sort-compare, functional/no-expression-statement
 array.splice(0, 1);
 
 // @ts-expect-error Mutations methods not allowed.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/require-array-sort-compare, ban/ban, functional/no-expression-statement
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/require-array-sort-compare, functional/no-expression-statement
 array.unshift("vangware");
