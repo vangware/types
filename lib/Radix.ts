1c7ee341e2c3300ca0382398b8ed0cbd7852930a
Luke Shiru <hello@lukeshiru.dev>
2022-08-05 03:11:42 -0700

    :arrow_up: dependency upgrade. :sparkles: add new `Radix` type.

---
+++ lib/Radix.ts
@@ -0,0 +1,47 @@
+/**
+ * Valid radix values (from `2` to `36`).
+ *
+ * @category Number
+ * @example
+ * ```typescript
+ * const binary: Radix = 2;
+ * const decimal: Radix = 10;
+ * const hexadecimal: Radix = 16;
+ * ```
+ */
+export type Radix =
+	| 2
+	| 3
+	| 4
+	| 5
+	| 6
+	| 7
+	| 8
+	| 9
+	| 10
+	| 11
+	| 12
+	| 13
+	| 14
+	| 15
+	| 16
+	| 17
+	| 18
+	| 19
+	| 20
+	| 21
+	| 22
+	| 23
+	| 24
+	| 25
+	| 26
+	| 27
+	| 28
+	| 29
+	| 30
+	| 31
+	| 32
+	| 33
+	| 34
+	| 35
+	| 36;
