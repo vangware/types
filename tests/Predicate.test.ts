02a63e9e88ef70efcc85d4f45aae5f3448bf024c
Lou Cyx <git@lou.cx>
2023-06-03 13:45:21 -0700

    🏗️ Split `Predicate` type into `Predicate` and `Filter`. 💥 Update `Comparison` to optionally use `Predicate` or `Filter`.

--- tests/Predicate.test.ts
+++ tests/Predicate.test.ts
@@ -1,14 +1,8 @@
 import type { Predicate } from "../src/Predicate.js";
 
-export const simplePredicate: Predicate<number> = input => input % 2 === 0;
 export const complexPredicate: Predicate<number | string, string> = (
 	input: number | string,
 ): input is string => typeof input === "string";
 
 // @ts-expect-error Output should be a boolean
-export const wrongPredicate: Predicate<number> = input => input;
-
-export const wrongComplexPredicate: Predicate<number | string> = (
-	input,
-	// @ts-expect-error Output should be a boolean
-): input is string => input;
+export const wrongPredicate: Predicate<number, number> = input => input;
